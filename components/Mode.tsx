d0b724230a0668ca56c619785620cf0f1548af84
vikashkhati007 <vikashkhati007@gmail.com>
2024-02-08 16:13:17 +0530

    starting now

---
+++ components/Mode.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Sun } from "lucide-react";
+const Mode = () => {
+  return (
+    <div>
+      <Sun className="w-10 h-10 p-1 border border-primary rounded-full bg-primary text-background" />
+    </div>
+  );
+};
+
+export default Mode;
