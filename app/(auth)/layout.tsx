22bef47e81e314549d1449e99d62381be6d2e940
vikashkhati007 <vikashkhati007@gmail.com>
2024-02-09 20:00:05 +0530

    first commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "@/app/globals.css";
+import { cn } from "@/lib/utils";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <section>{children}</section>;
+const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
+
+export const metadata: Metadata = {
+  title: "Auth Chat-Flow",
+  description:
+    " ChatFlow is a real-time messaging platform designed to facilitate seamless communication between users. With its intuitive interface and responsive design, ChatFlow enables users to engage in dynamic conversations effortlessly. Whether you're connecting with friends, collaborating with colleagues, or engaging with an online community.",
+  keywords: [
+    "chat",
+    "messaging",
+    "real-time",
+    "communication",
+    "chatflow",
+    "chat-flow",
+    "chat flow",
+  ],
 };
 
-export default layout;
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("bg-[#00072B]", poppins.className)}>{children}</body>
+    </html>
+  );
+}
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 
